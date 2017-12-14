2f97c9c9e871b8950e341228ac15364a87203e18
zhengcf <zhengcaifu@1bgx.net>
2017-12-14 10:49:08 +0800

    Init

--- app/src/main/java/com/test/caifu/view/SlidingMenu.java
+++ app/src/main/java/com/test/caifu/view/SlidingMenu.java
@@ -5,7 +5,6 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.view.WindowManager;
